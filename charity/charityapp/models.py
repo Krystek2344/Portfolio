da0fdcab5ab8b86556d7059ae918eb530bba2d6e
Krystek2344 <krystian.oprzala@gmail.com>
2022-01-16 18:10:24 +0100

    Add Donation model

--- charity/charityapp/models.py
+++ charity/charityapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -24,3 +25,17 @@ class Institution(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField(default=0)
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=155)
+    phone_number = models.CharField(max_length=25)
+    city = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=12)
+    pick_up_date = models.DateTimeField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField(max_length=400)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, default=None)
